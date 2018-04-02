70132a938f09182cc28a85db43d9f327874d64d2
Pete Lumbis <plumbis@cumulusnetworks.com>
2018-04-02 10:47:22 -0400

    create management network tests

--- tests/test_Main.py
+++ tests/test_Main.py
@@ -77,3 +77,44 @@ class TestMain(object):  # pylint: disable=W0612,R0903
             print "\n".join(diff_output)
 
             assert not diff_output
+
+    def test_create_mgmt_network(self): # pylint: disable=R0201
+        """Test creating a management network
+        """
+
+        testargs = ["reference_topology.py", "-c", "./tests/dot_files/simple.dot"]
+        expected_result_file = "./tests/expected_jinja_outputs/main_create_mgmt_network.vagrantfile"  # pylint: disable=C0301
+
+        # The list of helper script file names to test
+        helper_scripts = ["ansible_hostfile", "bridge-untagged", "dhcpd.conf",
+                          "hosts", "oob_server_config_auto_mgmt.sh", "ztp_oob.sh"]
+
+        with patch.object(sys, 'argv', testargs):
+
+            tc.main()
+
+            vagrant_output = open("Vagrantfile").read().splitlines()
+            expected_output = open(expected_result_file).read().splitlines()
+
+            # Get a diff of the files, makes troubleshooting errors easier
+            diff = list(difflib.Differ().compare(vagrant_output, expected_output))
+
+            diff_output = self.get_diff(diff)
+
+            print "\n".join(diff_output)
+
+            assert not diff_output
+
+            for filename in helper_scripts:
+                generated_helper_output = open(
+                    "helper_scripts/auto_mgmt_network/" + filename).read().splitlines()
+                expected_helper_output = open(
+                    "./tests/expected_helper_scripts/main_test_create_mgmt_network/" + filename).read().splitlines()  # pylint: disable=C0301
+
+                helper_diff = list(difflib.Differ().compare(generated_helper_output, expected_helper_output))  # pylint: disable=C0301
+
+                helper_diff_output = self.get_diff(helper_diff)
+
+                print "\n".join(diff_output)
+
+                assert not helper_diff_output
